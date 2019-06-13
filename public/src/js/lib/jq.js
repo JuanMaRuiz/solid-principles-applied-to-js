6262bc88a884880b84ec85aa1437a867e6215876
JuanMa Ruiz <ruizjuanma@gmail.com>
2019-06-13 19:44:28 +0200

    Created ajax compatible version

--- public/src/js/lib/jq.js
+++ public/src/js/lib/jq.js
@@ -2,9 +2,29 @@
   'use strict';
 
   function ajax(url) {
-    return fetch(url)
-      .then(response => response.json())
-      .then(data => data );
+    if (scope.fetch) {
+      return fetch(url)
+        .then(response => response.json())
+        .then(data => data );
+    } else {
+      return makeRequest(url);
+    }
+  }
+
+  function makeRequest(url) {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open("GET", url);
+      xhr.onload = () => {
+        if (xhr.status === 200) {
+          return resolve(JSON.parse(xhr.responseText));
+        } else {
+          return resolve(xhr.statusText);
+        }
+      };
+      xhr.onerror = () => reject(xhr.statusText);
+      xhr.send();
+    })
   }
 
   function $on(target, type, cb) {
